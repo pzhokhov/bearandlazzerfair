b3205524a49482ef47deaee20f2e96851180be60
Peter Zhokhov <peterz@openai.com>
2018-09-30 15:25:12 -0700

    flask and templates

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import flask
+
+app = flask.Flask('__name__')
+
+@app.errorhandler(404)
+def not_found(e):
+    return flask.render_template('404.j2'), 404
+
+@app.route('/')
+def index():
+    return flask.render_template('index.j2')
+
+
+def main():
+    app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    main()
