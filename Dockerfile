a6b821c17219b04740ae9b075b527fb482071cf5
peter <peterzhokhoff@gmail.com>
2018-10-14 22:19:32 -0700

    switch to gae

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.6
-
-COPY . /root/code
-WORKDIR /root/code
-
-RUN pip install -r requirements.txt
-CMD python app.py
---
+++ appengine_config.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import vendor
+# import tempfile
+# import subprocess
+
+# tempfile.SpooledTemporaryFile = tempfile.TemporaryFile
+# subprocess.Popen = None
+
+vendor.add('lib')
